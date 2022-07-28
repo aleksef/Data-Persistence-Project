9ec9f6f8e3ce8633290d9f2d04773d541be01b50
aleksef <38755134+aleksef@users.noreply.github.com>
2022-07-28 19:53:08 +0300

    add quit button

--- Assets/Scripts/StartMenuUiHandler.cs
+++ Assets/Scripts/StartMenuUiHandler.cs
@@ -5,6 +5,11 @@ using TMPro;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+[DefaultExecutionOrder(1000)]
 public class StartMenuUiHandler : MonoBehaviour
 {
     [SerializeField] TextMeshProUGUI usernameInputText;
@@ -17,5 +22,14 @@ public class StartMenuUiHandler : MonoBehaviour
             GameManager.Instance.username = usernameInputText.text;
         }
         SceneManager.LoadScene(1);
+    }
+
+    public void Quit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit(); // original code to quit Unity player
+#endif
     }
 }
